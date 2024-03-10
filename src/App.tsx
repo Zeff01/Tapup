3150fe6bd98a87ae50779a52daf4733e7f0adaed
nephrite <oneabove232@gmail.com>
2024-03-10 16:31:36 +0800

    added comment

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Root from "./pages/Root"
 const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="" element={<Root />}>
-        
+        {/* place your routes here so the navbar will be visible */}
       </Route>
     )
 )
