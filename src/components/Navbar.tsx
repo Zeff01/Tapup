3c15a354e25e875ae5250ffa5e06af41d29db602
nephrite <oneabove232@gmail.com>
2024-03-10 16:57:00 +0800

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,20 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
     return (
         <nav className="border-b flex flex-row justify-between text-lg font-bold">
-            <div className="flex flex-row gap-4 items-center">
-                <Link to={'/'}>
-                    <img src='./icon.png' className="scale-50"/>
-                </Link>
-                <Link to={'/'} className="py-2 px-8 flex flex-row gap-1">
+            <Link to={'/'} className="flex flex-row items-center">
+                <img src='./icon.png' className="scale-50" />
+                <p className="py-2 flex flex-row gap-1">
                     <span >Tap</span>
                     <span className="text-customGreen">Up</span>
-                </Link>
-            </div>  
+                </p>
+            </Link>  
             <div className="flex flex-row items-center gap-x-16 pr-20">
-                <Link className="py-2 px-8"  to={'/about'}>About</Link>
-                <Link className="py-2 px-8" to={'/services'}>Services</Link>
-                <Link className="py-2 px-8" to={'/testimonials'}>Testimonials</Link>
+                <Link className="py-2 px-8 hover:text-customGreen transition-all duration-150"  to={'/about'}>About</Link>
+                <Link className="py-2 px-8 hover:text-customGreen transition-all duration-150" to={'/services'}>Services</Link>
+                <Link className="py-2 px-8 hover:text-customGreen transition-all duration-150" to={'/testimonials'}>Testimonials</Link>
                 <div className="flex flex-row gap-4">
-                    <Link className="py-2 px-8" to={'/login'}>login</Link>
-                    <Link className="bg-black text-white py-2 px-8 rounded-sm" to={'/activate'}>activate</Link>
+                    <Link className="py-2 px-8 hover:text-customGreen transition-all duration-150" to={'/login'}>Log in</Link>
+                    <Link className="bg-black text-white py-2 px-8 rounded-sm hover:bg-customGreen hover:text-white transition-all duration-150" to={'/activate'}>Activate</Link>
                 </div>
             </div>          
         </nav>
